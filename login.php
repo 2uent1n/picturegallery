a218bd20577f5edb04af91e98b337641c1f6c676
DylanEst21 <dylan.esteves21@gmail.com>
2025-12-10 16:23:59 +0100

    Add CSRF token to session on login page
    Implement CSRF token generation for session security.

--- login.php
+++ login.php
@@ -2,6 +2,12 @@
 
 $page_title = 'Login';
 
+session_start();
+
+if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
 include 'includes/header.html';
 
 include 'includes/navbar.html';
