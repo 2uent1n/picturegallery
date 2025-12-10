8134da38df0462d5c6815c28617051b742acca0e
Ibrahim Diallo <ibrahim.diallo@edu.ece.fr>
2025-12-10 11:54:40 +0100

    fix: correction SLI

--- check_registration.php
+++ check_registration.php
@@ -17,24 +17,39 @@ else{
     $password = $_POST['pass'];
 
 
-    $sql1 = "SELECT users_username, users_password FROM users WHERE users_username = '{$username}' AND users_password = '{$password}'";
-    $sql2 = "INSERT INTO users (users_username, users_password) VALUES ('$username', '$password')";
+    $sql1 = "SELECT users_username, users_password 
+             FROM users 
+             WHERE users_username = ? AND users_password = ?";
 
-    $result1 = mysqli_query ($connection, $sql1) or die (mysqli_error ($connection));
+    $stmt1 = mysqli_prepare($connection, $sql1);
+    mysqli_stmt_bind_param($stmt1, "ss", $username, $password);
+    mysqli_stmt_execute($stmt1);
+    $result1 = mysqli_stmt_get_result($stmt1);
+
 
     if (mysqli_num_rows ($result1) == 0){
-        if (mysqli_query ($connection, $sql2)){
+
+        $sql2 = "INSERT INTO users (users_username, users_password) 
+                 VALUES (?, ?)";
+
+        $stmt2 = mysqli_prepare($connection, $sql2);
+        mysqli_stmt_bind_param($stmt2, "ss", $username, $password);
+
+        if (mysqli_stmt_execute($stmt2)){
             include 'includes/new_registration.php';
         }
         else{
             include 'includes/error.php';
-            
         }
+
+
+        mysqli_stmt_close($stmt2);
 
     }
     else{
         include 'includes/notregistered.php';
     }
+    mysqli_stmt_close($stmt1);
 }
 
 mysqli_close ($connection);
